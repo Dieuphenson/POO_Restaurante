b93c9205ae88bf6879c602f1d98f8f43a09d75a2
Dieuphenson Jean <147110592+Dieuphenson@users.noreply.github.com>
2023-11-19 20:17:17 -0300

    Add files via upload

---
+++ Pedido.java
@@ -0,0 +1,56 @@
+package com.Restaurante.api.atendimento;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long numeroPedido;
+	
+	@Column(nullable=false)
+	private String nome;
+
+	public Long getNumeroPedido() {
+		return numeroPedido;
+	}
+
+	public void setNumeroPedido(Long numeroPedido) {
+		this.numeroPedido = numeroPedido;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(numeroPedido);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(numeroPedido, other.numeroPedido);
+	}
+
+	
+}
